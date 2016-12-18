bc3e57f0a5a14465d60f850c96db5a279751d037
James Thomas <james_thomas@apple.com>
2016-12-18 11:07:12 -0600

    updated make view, not yet functional

--- resources/views/task/make.blade.php
+++ resources/views/task/make.blade.php
@@ -10,5 +10,40 @@
 
 @section('content')
 
-    <p>Fun fact: this does nothing</p>
+    <h2>Make a task:</h2>
+
+    <form method='POST' action='/save'>
+
+      {{ csrf_field() }}
+
+      <div class='form-group'>
+        <label>Description</label>
+          <input
+              type='text'
+              id='description'
+              name='description'
+              value='{{ old('description', 'Things to do') }}'
+          >
+          <div class='error'>{{ $errors->first('destription') }}</div>
+      </div>
+
+      <div class='form-group'>
+        <label>Complete</label>
+          <input
+              type='checkbox'
+              id='complete'
+              name='complete'
+              value='No'
+          >
+      </div>
+
+      <div class='form-instructions'>
+        Description is required
+      </div>
+
+      <button type='submint'>Make this task</button>
+
+
+    </form>
+
 @endsection
